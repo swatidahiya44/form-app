f175e54d464f34c7c70a879b5c93d4c2930fefdc
swatidahiya44 <swatidahiya44@gmail.com>
2021-02-17 01:40:49 +0530

    template-driven

--- src/app/view/my-form/my-form.component.ts
+++ src/app/view/my-form/my-form.component.ts
@@ -1,60 +1,24 @@
-import { NgStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-//import { FormService } from '../form.service';
 import { Feedback } from 'src/app/models/feedback.model';
 import { FormService } from 'src/app/controllers/form.service';
-
 
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
   styleUrls: ['./my-form.component.css']
 })
-export class MyFormComponent implements OnInit {
-  registerForm: FormGroup;
-  submitted = false;
-  feedback: Feedback;
-  constructor(private formBuilder: FormBuilder, private httpClient : HttpClient, private formService: FormService) {
-    
-   }
-
-
-  ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      designation: ['', Validators.required],
-      companyName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-      mobileNumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
-      comment: ['', []]
-  });
-  }
+export class MyFormComponent {
+  feedback: Feedback = {} as Feedback;
+  constructor(private formService: FormService) {}
   
-  get f() { return this.registerForm.controls; }
-
   onSubmit() {
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-        return;
-    }
-    
-    //console.log(this.feedback.mobileNumber);
-    //this.httpClient.post("/api/feedback", this.feedback).subscribe((resp)=>{
-    //  console.log(resp)
-    //});
     console.log(this.feedback);
-
     this.formService.postFeedback(this.feedback).subscribe((resp) => {
       console.log(resp);
       alert("feedback has been posted");
-    })
-
-
-
-    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
-}
+    }, (error)=> {
+      alert("Not able to save!!")
+    });
+  }
 
 }
